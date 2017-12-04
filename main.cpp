9ed7696bc8d6c2329d28e051ad1e5b822593ed88
ChuanCong Xue <xuechuancong@gmail.com>
2017-12-04 23:17:45 +0800

    数组相关算法

--- main.cpp
+++ main.cpp
@@ -5,6 +5,197 @@ using namespace std;
 class Solution {
 public:
 
+//    三路排序使用
+    void sortColors2(vector<int>& nums) {
+        int n = static_cast<int>(nums.size());
+
+        int index1 = -1, index2 = n;
+        int i = 0;
+        while (i < index2 ) {
+            if (nums[i] == 0 ) {
+                index1 ++;
+                swap(nums[i], nums[index1]);
+                i ++;
+            }
+            else if(nums[i] == 1) {
+                i ++;
+            }
+            else if( nums[i] == 2) {
+                index2 --;
+                swap(nums[i], nums[index2]);
+            }
+
+        }
+    }
+
+//    基数排序使用
+    void sortColors1(vector<int>& nums) {
+        int n = static_cast<int>(nums.size());
+
+        vector<int> count(3, 0);
+        for (int i = 0; i < n; ++i) {
+            count[ nums[i] ] ++;
+        }
+
+        int index = 0;
+        for (int j = 0; j < count[0]; ++j) {
+            nums[index] = 0;
+            index ++;
+        }
+
+        for (int k = 0; k < count[1]; ++k) {
+            nums[index] = 1;
+            index ++;
+        }
+
+        for (int l = 0; l < count[2]; ++l) {
+            nums[index] = 2;
+            index ++;
+        }
+    }
+
+    void sortColors(vector<int>& nums) {
+//        基数排序
+        int n = static_cast<int>(nums.size());
+
+        vector<int> count(3, 0);
+        for (int i = 0; i < n; ++i) {
+            count[ nums[i] ] ++;
+        }
+
+        int index = 0;
+//      改进版本
+        for (int m = 0; m < count.size(); ++m) {
+            for (int i = 0; i < count[m]; ++i) {
+                nums[index] = m;
+                index ++;
+            }
+        }
+
+
+    }
+
+    int removeDuplicates1(vector<int>& nums) {
+        int n = static_cast<int>(nums.size());
+
+        //k是已经无重复数组的最后元素索引
+        int k = -1;
+        for (int i = 0; i < n; ++i) {
+
+//            处理一下开始元素
+            if(i < 1 || nums[i] != nums[k] ) {
+                k++;
+                swap(nums[ k ], nums[i]);
+
+//                第一次交换完成后检查是否可以进行第二次重复元素交换
+                if( nums[i+1] == nums[k] ) {
+                    i ++;
+                    k ++;
+
+                    swap(nums[i], nums[k]);
+                }
+            }
+        }
+
+        for (int j = k+1; j < n; ++j) {
+            nums.pop_back();
+        }
+
+        return static_cast<int>(nums.size());
+    }
+
+    int removeDuplicates(vector<int>& nums) {
+
+        int n = static_cast<int>(nums.size());
+
+        //k是已经无重复数组的最后元素索引
+        int k = 0;
+        for (int i = 1; i < n; ++i) {
+            if( nums[i] != nums[k] ) {
+                k++;
+                swap(nums[ k ], nums[i]);
+            }
+        }
+
+        for (int j = k+1; j < n; ++j) {
+            nums.pop_back();
+        }
+
+        return static_cast<int>(nums.size());
+    }
+
+    int removeElement(vector<int>& nums, int val) {
+        int n = static_cast<int>(nums.size());
+        int k = 0;
+        for (int i = 0; i < n; ++i) {
+            if(nums[i] != val ) {
+                swap(nums[i], nums[k]);
+                k++;
+            }
+        }
+
+        for (int j = k; j < n; ++j) {
+            nums.pop_back();
+        }
+
+        return static_cast<int>(nums.size());
+    }
+
+    void moveZeroes2(vector<int>& nums) {
+
+        int k = 0;
+        for (int i = 0; i < nums.size(); ++i) {
+            if(nums[i] ) {
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+
+        for (int j = k; j < nums.size(); ++j) {
+            nums[j] = 0;
+        }
+    }
+
+//    时间复杂度O(n)
+//      空间复杂度O(1)
+    void moveZeroes1(vector<int>& nums) {
+
+        int k = 0;
+        for (int i = 0; i < nums.size(); ++i) {
+            if(nums[i] ) {
+                //nums[k] = nums[i];
+                //如果数组中基本没有0(i == k)的话怎么优化？
+                if( i != k)
+                    swap(nums[i], nums[k ++]);
+                else
+                    k++;
+            }
+        }
+
+
+    }
+
+
+//        使用辅助空间
+    void moveZeroes(vector<int>& nums) {
+
+        vector<int> newList;
+
+        for (int i = 0; i < nums.size(); ++i) {
+            if( nums[i] ) {
+                newList.push_back(nums[i]);
+            }
+        }
+
+        for (int j = 0; j < newList.size(); ++j) {
+            nums[j] = newList[j];
+        }
+
+        for (int l = static_cast<int>(newList.size()); l < nums.size(); ++l) {
+            nums[l] = 0;
+        }
+    }
+
 // 对于一个有序循环数组arr，返回arr中的最小值。
 // 有序循环数组是指，有序数组左边任意长度的部分放到右边去，右边的部分拿到左边来。
 // 比如数组[1,2,3,3,4]，是有序循环数组，[4,1,2,3,3]也是。
